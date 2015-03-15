66423427cfe8d9d694350d889cd91c1f9f686e33
J-Lemon <akira.zeta@gmail.com>
2015-03-15 01:58:47 +0100

    added About page

--- gatecontrol/gatecontrol.py
+++ gatecontrol/gatecontrol.py
@@ -10,19 +10,17 @@ STATE_PENDING = {'id' : 2, 'description' : 'pending'}
 
 
 class Gate:
-    
+
     def __init__(self):
         self.state = STATE_CLOSED
-    
+
     def open_gate(self, request=None):
         if request is not None:
             self.state = STATE_OPEN
             request.done()
-    
+
     def get_state(self, request=None):
         return self.state
-    
+
     def get_available_states(self):
         return (STATE_OPEN, STATE_CLOSED)
-    
-  
